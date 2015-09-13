b1f9884a6182bf74e1db124412b0632e756f3346
thomas_jakemeyn <thomas.jakemeyn@gmail.com>
2015-09-13 12:36:08 +0200

    Upgraded to latest version of jspm, babel, babel-runtime and core-js

--- config.js
+++ config.js
@@ -1,30 +1,28 @@
 System.config({
-  "defaultJSExtensions": true,
-  "transpiler": "babel",
-  "babelOptions": {
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
     "optional": [
       "runtime",
       "es7.decorators",
       "es7.classProperties"
     ]
   },
-  "paths": {
+  paths: {
     "*": "*",
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
-  }
-});
+  },
 
-System.config({
-  "map": {
+  map: {
     "aurelia-binding": "github:aurelia/binding@0.8.5",
     "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.9.1",
     "aurelia-router": "github:aurelia/router@0.10.4",
     "aurelia-templating": "github:aurelia/templating@0.13.16",
-    "babel": "npm:babel-core@5.5.2",
-    "babel-runtime": "npm:babel-runtime@5.5.2",
+    "babel": "npm:babel-core@5.8.24",
+    "babel-runtime": "npm:babel-runtime@5.8.24",
     "bluebird": "npm:bluebird@2.9.34",
-    "core-js": "npm:core-js@0.9.18",
+    "core-js": "npm:core-js@1.1.4",
     "github:aurelia/binding@0.8.5": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.9.1",
       "aurelia-metadata": "github:aurelia/metadata@0.7.2",
@@ -75,7 +73,7 @@ System.config({
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "npm:babel-runtime@5.5.2": {
+    "npm:babel-runtime@5.8.24": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:bluebird@2.9.34": {
@@ -85,7 +83,11 @@ System.config({
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:core-js@1.1.4": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     }
   }
 });
-
